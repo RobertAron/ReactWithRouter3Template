bf2861ca16988dd27ffbade742ea2ebf6edccf83
HeyImRige <Robert.aron.tx@verizon.net>
2017-03-31 19:00:44 -0500

    initial setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+import HomePage from './containers/HomePage';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+
+
+ReactDOM.render((
+    <Router>
+      <div>
+          <Route exact path="/" component={App}/>
+          <Route path="/home" component={HomePage}/>
+      </div>
+    </Router>
+  ),
+  document.getElementById('root')
+);
